f8ca9017d6a46334e19b8bbe9bde388ca6686598
dipak921 <dipakchande921@gmail.com>
2025-11-05 14:17:38 +0530

    Add project files.

---
+++ Learning_C_Sharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome dot net developer");
+            //Console.WriteLine("Enter Your Name");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Enter Your Address");
+            //string Address = Console.ReadLine();
+
+            //// three type of concate 
+            ////Console.Write("My Name is " + name + "My address is " + Address); // Cocatination
+
+            ////Console.WriteLine("my name is {0} and my address is {1}",name, Address); // Placeholder 
+
+            //Console.WriteLine($"my name is {name} and my address is {Address}"); // strin interpolatin
+
+            Console.WriteLine("Entre a no. 1");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Entre a no. 2");
+            int num2 = int.Parse(Console.ReadLine()); // convert read line to  int read line is 
+
+            int total = num1 + num2;
+            Console.WriteLine($"Sum of {num1} and {num2} = {total}");
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Learning_C_Sharp/datatype.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datatype
+{
+    internal class datatype
+    {
+        static void Main(string[] args)
+        {
+            // Value type
+            int age = 25; // Integer type
+            float height = 5.9f; // Floatin type  - 7
+            double weight = 0.12;
+            char grade = 'A';
+            bool isStudent = true;
+
+            // Refernce Type
+            string name = "Dipak";
+
+            // sotre any thing 
+            object anything = 230000;
+
+
+            Console.WriteLine($"my age is {age} and my height is {height}and weight is {weight} grade is {grade} Me is pass  {isStudent} my name is {name} my bank blance is {anything}");
+
+            Console.ReadLine();
+        }
+    }
+}
