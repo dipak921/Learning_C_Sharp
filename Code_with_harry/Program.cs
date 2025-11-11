411e4c5a8f30d0033e8967234fab74939dfb897f
dipak921 <dipakchande921@gmail.com>
2025-11-11 16:20:50 +0530

    Arrya functon

---
+++ Code_with_harry/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_with_harry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Type Casting
+            // There are two types of casting
+            // 1. Implicit Casting
+            // Char to int to long to float to double
+
+            // 2. Explicit Casting
+
+            // int a = (int) 3.5;
+            //int x = 3;
+            //double y = x;
+            //int z = 'y';
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+
+            // Math Class
+
+            int a = Math.Max(34, 65);
+            Console.WriteLine(a);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Learning_C_Sharp/Array.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_C_Sharp
+{
+    internal class Array
+    {
+        static void Main(string[] args)
+        {
+            //int[] number = { 10, 20, 30, 40, 50,60,70,80,90 };
+            //Console.WriteLine("Array Element ");
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    Console.WriteLine($"Elements of index {i} = {number[i]}");
+            //}
+
+
+            // Enter a marks in Display 
+            //int[] marks = new int[5];
+            //Console.WriteLine("Enter a marks of out 100");
+            //for (int i = 0; i < marks.Length; i++)
+            //{
+            //    Console.WriteLine($"Enter Marks {i + 1}");
+            //    marks[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("Enterd a mark :");
+            //for (int i = 0; i < marks.Length; i++)
+            //{
+            //    Console.WriteLine($"Subject {i + 1} : {marks[i]}");
+            //}
+
+
+
+            //string[] names = { "Dipak", "Dinesh", "Jayant", "Shubham", "Yash", "Mahesh" };
+            //Console.WriteLine("Names in array");
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+
+            //object[] mix = {1,"Dipak",65.24,true};
+            //Console.WriteLine("Mixed Array");
+            //for (int i = 0; i < mix.Length; i++)
+            //{
+            //    Console.WriteLine(mix[i]);
+            //}
+
+
+            //int[,] number = {
+            //    {1,2,3},
+            //    {4,5,6},
+            //    {7,8,9},
+            //};
+            //for(int i = 0; i < 3; i++)
+            //{
+            //    for(int j = 0; j < 3; j++)
+            //    {
+            //        Console.Write(number[i,j] + "\t " );
+            //    }
+            //    Console.WriteLine();
+            }
+
+        //  enter mark form user mark 2 student three subject
+        int[,] marks = new int[2, 3];
+
+        Console.WriteLine("Enter marks for 2 students (3 subjects each):");
+
+for (int i = 0; i< 2; i++)
+{
+    Console.WriteLine($"\nStudent {i + 1}:");
+    for (int j = 0; j< 3; j++)
+    {
+        Console.Write($"Enter marks for Subject {j + 1}: ");
+        marks[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("\nMarks Entered:");
+
+for (int i = 0; i < 2; i++)
+{
+    Console.Write($"Student {i + 1}: ");
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write(marks[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadLine();
+
+
+
+Console.ReadLine();
+        }
+    }
+}
