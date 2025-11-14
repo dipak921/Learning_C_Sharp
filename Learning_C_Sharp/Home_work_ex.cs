7b7d607637ac38d9e7727e6307024bb2ef197dfe
dipak921 <dipakchande921@gmail.com>
2025-11-15 02:18:34 +0530

    Home work Complted

---
+++ Learning_C_Sharp/Home_work_ex.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_C_Sharp
+{
+    internal class Home_work_ex
+    {
+        public void Greating()
+        {
+            Console.Write("Enter Customer Name :");
+            string custName = Console.ReadLine();
+            Console.WriteLine($"Welcome ,{custName}");
+            Console.WriteLine("----------Dipak’s Urban Mall ----------");
+        }
+        public  void Item(string itemName, int price)
+        {
+            Console.WriteLine($"Selected item :{itemName} Price : {price} per unit");
+        }
+
+        public  int totalAmount()
+            {
+            Console.Write("Entre a Price per unit :");
+            int price = int.Parse(Console.ReadLine());
+            Console.Write("Entre a Quntity :");
+            int qnt = int.Parse(Console.ReadLine());
+            double total = price * qnt;
+            Console.WriteLine($"Total (without discount): {total}");
+            return(int) total;
+            }
+
+        public double FinalPay(double discountPercent, double totalAmount)
+        {
+            double discountAmount = (discountPercent / 100) * totalAmount;
+            double finalAmount = totalAmount - discountAmount;
+            return finalAmount;
+        }
+
+
+        static void Main(string[] args)
+        {
+            //1
+            Home_work_ex great = new Home_work_ex();
+            great.Greating();
+
+            //2
+            Console.Write("Enter Item name :");
+            string itemName = Console.ReadLine();
+
+            Console.Write("Enter price : ");
+            int price = int.Parse( Console.ReadLine());
+            great.Item(itemName, price);
+            //3
+            great.totalAmount();
+            //4
+           
+            Console.Write("Enter discount percent: ");
+            double discount = double.Parse(Console.ReadLine());
+            Console.Write("Enter total amount : ");
+            double totalAmount = double.Parse(Console.ReadLine());
+
+            double finalPrice = great.FinalPay(discount, totalAmount);
+            Console.WriteLine($"Final Amount after discount: {finalPrice}");
+
+            Console.Write("thank you for shopping");
+
+            Console.ReadLine();
+        }
+    }
+}
--- Learning_C_Sharp/non_static_function.cs
+++ Learning_C_Sharp/non_static_function.cs
@@ -37,6 +37,7 @@ namespace Learning_C_Sharp
         {
             //non_static_function ft = new non_static_function();
             //ft.addtion();
+            non_static_function obj = new non_static_function();
 
             Console.WriteLine("enter a number 1 ");
             int x = Convert.ToInt32(Console.ReadLine());
@@ -45,9 +46,10 @@ namespace Learning_C_Sharp
 
             function_type.subtraction(x,y);
 
-            
 
-            function_type.multiplicaton();
+            int result = obj.multiplicaton();
+
+            //function_type.multiplicaton();
 
 
         }
