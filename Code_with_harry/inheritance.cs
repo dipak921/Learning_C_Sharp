2dc1c7035aef0057a0efd2ddee65bee2c7a65fca
dipak921 <dipakchande921@gmail.com>
2025-11-19 12:59:26 +0530

    Inheritance type

---
+++ Code_with_harry/inheritance.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Inheritance
+//-->The similarity in physical features of a child to that of its parents is due to the child having inherited these features from its parents.
+//--> Similarly, in c# inheritance allows you to create a class by deriving the common attributes and method of an existing class.
+//--> Inheritance provides reusability by allowing us to extend an exsting class.
+//-->The reason behind oop programmin it to promote the reusabiliity of code and to reduce complexity in code and it is possible by using inheritance.
+
+//BASE CLASS
+//--> IS THE    CLASS from which features are to be inherited into another class
+//--> it is the class in which the base class features are inherited.
+
+
+
+
+class employees
+{
+    public int empid;
+    public string name;
+    public int age;
+    public int contactNumber;
+
+}
+class visitingemployees : employees
+{
+    public int visitingSalary;
+    public int visitingHourse;
+
+}
+class permentemployees : employees
+{
+    public int ParmentSalary;
+    public int ParmentHourse;
+}
+
+namespace Code_with_harry
+{
+    internal class inheritance
+    {
+        static void Main(string[] args)
+        {
+            permentemployees dipak = new permentemployees();
+            dipak.age = 10;
+            Console.WriteLine(dipak.age);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Code_with_harry/inheritance_type.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//TYPE OF INHERITANCE 
+// 1. SINGLE
+// --> It is the type of inheritance in which there is one base class and one derived class
+//               SYNTAX
+//              class a (base class)
+//               {
+
+//               }
+//              class b:a (derived class)
+//               {
+
+//               }
+//--------------------------------------------------------------------------
+// 2. HIERACHICAL
+//--> This is the type of inheritance in which there are multiple classes derived from one base class.
+//--> This type of inheritance is used when there is a requirement of one class feature that is needed in multiple classes.
+//               SYNTAX
+//              class a (base class)
+//               {
+
+//               }
+//              class b :a (derived class)
+//               {
+
+//               }
+//              class b :a (derived class)
+//               {
+
+//               }
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------
+// 3. MULTI-LEVEL
+//--> when one class is derived from another derived class then this type of inheritance is called multilevel inheritance.
+//                                  SYNTAX
+//                      class A  // Base class
+//                       {
+//   
+//                        }
+//                      class B : A  // Derived from A
+//                         {
+
+//                         }
+//                        class C : B  // Derived from B
+//                         {
+
+//                         }
+
+// 4. MULTIPLE (USING INTERFACE)
+//--> when
+
+
+
+namespace Code_with_harry
+{
+    // single inheritance
+    //class parent
+    //{
+    //    public void show()
+    //    {
+    //        Console.WriteLine("Parent class/ base class");
+    //    }
+    //}
+    //class child : parent
+    //{
+    //    public void show1()
+    //    {
+    //        Console.WriteLine("Child class / dervied class");
+    //    }
+    //}
+
+    // end single in heritance
+
+    //----------------HIERACHICAL
+    //class parent
+    //{
+    //    public void show()
+    //    {
+    //        Console.WriteLine("Parent class/ base class");
+    //    }
+    //}
+    //class child : parent
+    //{
+    //    public void show1()
+    //    {
+    //        Console.WriteLine("Child frist class / dervied first class");
+    //    }
+    //}
+    //class child1 : parent//,child
+    //{
+    //    public void show2()
+    //    {
+    //        Console.WriteLine("Child second class / dervied second class");
+    //    }
+    //}
+
+    //----------------Multilevel inherritance
+
+    //   class parent
+    //{
+    //    public void show()
+    //    {
+    //        Console.WriteLine("Parent class/ base class");
+    //    }
+    //}
+    //class child : parent
+    //{
+    //    public void show1()
+    //    {
+    //        Console.WriteLine("Child frist class / dervied first class");
+    //    }
+    //}
+    //class child1 : child
+    //{
+    //    public void show2()
+    //    {
+    //        Console.WriteLine("Child second class / dervied second class");
+    //    }
+    //}
+
+    // 4. MULTIPLE (USING INTERFACE)
+
+    internal class inheritance_type
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("------------------Sigle inheritance------------------");
+            //child ch = new child();
+            //ch.show();
+            //ch.show1();
+
+            //Console.WriteLine("------------------HIERACHICAL inheritance------------------");
+
+            //child1 ch1 = new child1();
+            //ch1.show();
+            //ch1.show2();
+
+            //Console.WriteLine("------------------Multilevel inheritance------------------");
+            //child1 ch1 = new child1();
+            //ch1.show();
+            //ch1.show1();
+            //ch1.show2();
+
+            Console.WriteLine("------------------ MULTIPLE (USING INTERFACE)------------------");
+
+
+            Console.ReadLine();
+        }
+    }
+}
