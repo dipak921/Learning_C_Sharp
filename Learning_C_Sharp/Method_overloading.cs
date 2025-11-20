20700af4ec4967e9f7d2cf7779858c9c78338a27
dipak921 <dipakchande921@gmail.com>
2025-11-20 16:05:56 +0530

    Polymorphism

---
+++ Learning_C_Sharp/Method_overloading.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_C_Sharp
+{
+    internal class Method_overloading // dyanamic compaile time
+    {
+
+        public void Addition()
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine($"sum is : {c}");
+        }
+        public void Addition(int a , int b)
+        {
+            int c = a + b;
+            Console.WriteLine($"sum is : {c}");
+        }
+        public void Addition(int a, int b,int e)
+        {
+            int c = a + b;
+            Console.WriteLine($"sum is : {c}");
+        }
+        public void Addition(int a, double b)
+        {
+            double c = a + b;
+            Console.WriteLine($"sum is : {c}");
+        }
+        public void Addition(string a, string b)
+        {
+            string c = a + b;
+            Console.WriteLine($"sum is : {c}");
+        }
+        // HOME WORK
+        public void bookroom(string roomtype)
+        {
+            Console.WriteLine($"Booked room :{roomtype}");
+        }
+        public void bookroom(int guest, string roomtype)
+        {
+            Console.WriteLine($"gust is:{guest} room type is : {roomtype}");
+        }
+        public void bookroom( string roomtype, int guest, Boolean extrapaid)
+        {
+            Console.WriteLine($"Booked room type is :{roomtype} guest is : {guest} extra paid :{extrapaid}");
+        }
+        static void Main(string[] args)
+        {
+            Method_overloading p = new Method_overloading();
+            //p.Addition();    // No parameters
+            //p.Addition(12,23); // int, int
+            //p.Addition(13,24,45); // int, int ,int
+            //p.Addition(13,2.4);// int, float
+            //p.Addition("dipak", "chande"); // string, string
+
+            p.bookroom("super deleax");
+            p.bookroom(4,"lexury");
+            p.bookroom("delex",1,true);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Learning_C_Sharp/method_overriding.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_C_Sharp
+{
+    //class Device
+    //{
+    //    public virtual void TureOn()
+    //    {
+    //        Console.WriteLine("Turning on Generiv Device");
+    //    }
+    //}
+    //class Tv : Device
+    //{
+    //    public override void TureOn()
+    //    {
+    //        Console.WriteLine("Tv is on now, showing channeel ");
+    //    }
+    //}
+    //class Ac : Device
+    //{
+    //    public override void TureOn()
+    //    {
+    //        Console.WriteLine("AC is on now, cooling now");
+    //    }
+    //}
+    //class music_system : Device
+    //{
+    //    public override void TureOn()
+    //    {
+    //        Console.WriteLine("Music system is on now, enjoy now");
+    //    }
+    //}
+
+    //--------------------------------With parameter--------------------------------------
+    class Device
+    {
+        public virtual void TureOn(string userName, string userType)
+        {
+            Console.WriteLine($"User Name is :{userName} User Type is: {userType} ");
+        }
+    }
+    class Tv : Device
+    {
+        public override void TureOn(string userName, string userType)
+        {
+            Console.WriteLine($"User Name is :{userName} User Type is: {userType} ");
+        }
+    }
+    class Ac : Device
+    {
+        public override void TureOn(string userName, string userType)
+        {
+            Console.WriteLine($"User Name is :{userName} User Type is: {userType} ");
+        }
+    }
+    class music_system : Device
+    {
+        public override void TureOn(string userName, string userType)
+        {
+            Console.WriteLine($"User Name is :{userName} User Type is: {userType} ");
+        }
+    }
+    internal class method_overriding
+    {
+        static void Main(string[] args)
+        {
+            //Device r = new Tv();
+            //r.TureOn();
+            //Device r1 = new Ac();
+            //r1.TureOn();
+            //Device r2 = new music_system();
+            //r2.TureOn();
+
+            //second example
+            Device de = new Device();
+            de.TureOn("dipak", "device oprator");
+            Device r = new Tv();
+            r.TureOn("dipak", "TV oprator");
+            Device r1 = new Ac();
+            r1.TureOn("dipak", "AC oprator");
+            Device r2 = new music_system();
+            r2.TureOn("dipak", "Music oprator");
+            Device r3 = new music_system();
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
