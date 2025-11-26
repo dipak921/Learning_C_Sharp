fd9450a928a4bc6b9aca1a9b05dd1a9f59fd9878
dipak921 <dipakchande921@gmail.com>
2025-11-27 00:31:28 +0530

    Update file abstraction

--- Learning_C_Sharp/Abstraction_salary_example.cs
+++ Learning_C_Sharp/Abstraction_salary_example.cs
@@ -4,6 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//         ABSTRACTION 
+// --> Abstraction is one of the principle of object oriented programing.
+// It is used to display only nesessary and essential features of an object to outside the world.
+// --> Means displaying what is necessary and encapsulate the unnecessary things to outside the world.
+// Hiding can be achieved by using "private" access modifier.
+//--> Abstraction is process of hiding the implementation details from the user only the functionality will be provided to the user.
+//--> In other words, the user will have the information on what the object does instead of how it does it .
+//--------------NOTES
+//--> Outside the world menas when we use reference of object then it will show only necessary methods
+//and properties (ABSTRACTION) and hide methods which are not necessary (ENCAPSULATION).
+
+// Abstraction
+// 1. Abstraction is "To represent the essential feature without representing the backgound details.
+// 2. Abstraction lets you focus on what the object does instead of how it does it.
+// 3. Abstraction provides you a generalized view of your classes or objects by providing relevant information
+// 4. Abstraction is the process of hiding the working style of an object, and showing the information
+// of an object in an understandable manner.
+
+
 namespace Learning_C_Sharp
 {
     class Employee
