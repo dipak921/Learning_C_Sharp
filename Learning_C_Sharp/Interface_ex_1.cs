3c9467d9b066dd1ca47545abf23dc636b5d45756
dipak921 <dipakchande921@gmail.com>
2025-12-01 09:01:25 +0530

    Add a interface notes

--- Learning_C_Sharp/Interface_ex_1.cs
+++ Learning_C_Sharp/Interface_ex_1.cs
@@ -6,6 +6,32 @@ using System.Threading.Tasks;
 
 namespace Learning_C_Sharp
 {
+    /*
+    --------------------- INTERFACES IN C#--------------------
+An interface is a contract between itself and any class that implements it.
+This contract states that any class that implements the interface will implement the interface's properties, methods and/or events. An interface contains no implementation,
+only the signatures of the functionality the interface provides. An interface can contain signatures of methods, properties, indexers & events.
+The interface defines the 'what' part of the syntactical contract and the deriving classes define the 'how' part of the syntactical contract.
+Interfaces define properties, methods, and events, which are the memb of the interface. Interfaces contain only the declaration of the members. I the responsibility
+    of the deriving class to define the members.
+One of the main reason to introduce interfaces is that it can be used in multiple inheritance.
+-> An interface contains only abstract members, just like classes interface also contains properties, methods, deregates or events, but only declarations, no implementations.
+-> An interface cannot be instantiated but can only be inherited by classes or other interfaces.
+-> Interface cannot have fields.
+-> An interface is declared using the keyword interface.
+-> In C#, by default, all members declared in an interface have public as the access modifier. They don't allow explicit access modifiers.
+     
+    -----------------IMPLEMENTING AN INTERFACE----------------------
+-> An interface is implemented by a class in a way similar to inheriting a class.
+-> When implementing an interface in a class, implement all the abstract methods declared in the interface. If all the methods are not implemented, the class cannot be compiled.
+-> The methods implemented in the class should be declared with the same name and signature as defined in the interface.
+
+     */
+
+    //    "I" is a prefix before employee that refers to an interface.
+    //    All member of an interface are public and abstract by default.
+    // All members of an interface is "Abstract" by default but after C# 8 we can create method with default implementation.
+    // No need of Explicit "Public" Access modifier
     interface IPrinter
     {
         void Print(string doucument, int pages);
