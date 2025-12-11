fcee1b4f1417d0cc7bad8673d56bfa8709da35b3
dipak921 <dipakchande921@gmail.com>
2025-12-11 20:42:53 +0530

    Star Pattern

---
+++ Learning_C_Sharp/ReverseStarPattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class ReverseStarPattern
+{
+    static void Main()
+    {
+        Console.Write("Enter number of rows: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        for (int i = rows; i >= 1; i--)
+        {
+            // Print spaces
+            for (int j = 1; j <= rows - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Learning_C_Sharp/StarPattern.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_C_Sharp
+{
+    internal class StarPattern
+    {
+        static void Main()
+        {
+            Console.Write("Enter number of rows: ");
+            int rows = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= rows; i++)
+            {
+                // Print spaces
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Print stars
+                for (int k = 1; k <= (2 * i - 1); k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
