703c14715144f547af9cb5b968fb13118b7dfb79
dipak921 <dipakchande921@gmail.com>
2025-12-23 23:46:46 +0530

    practice_question

---
+++ Learning_C_Sharp/practice_question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class practice_question
+{
+    // Function to calculate power of a number
+    static int Power(int baseNum, int exponent)
+    {
+        int result = 1;
+        for (int i = 1; i <= exponent; i++)
+        {
+            result *= baseNum;
+        }
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Input Base number: ");
+        int baseNum = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input the Exponent: ");
+        int exponent = Convert.ToInt32(Console.ReadLine());
+
+        int powerResult = Power(baseNum, exponent);
+
+        Console.WriteLine($"So, the number {baseNum} ^ (to the power) {exponent} = {powerResult}");
+    }
+}
