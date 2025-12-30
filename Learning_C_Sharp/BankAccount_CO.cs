01eeb247cf30fd7eacdde5118d56696e5bcb0644
dipak921 <dipakchande921@gmail.com>
2025-12-30 15:51:09 +0530

    Class and object

---
+++ Learning_C_Sharp/BankAccount_CO.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_C_Sharp
+{
+    internal class BankAccount_CO
+    {
+        class BankAccount1
+        {
+            public int accountNumber;
+            public string accountHolderName;
+            public double balance;
+        
+            public void Deposit(double amount)
+            {
+                balance += amount;
+                Console.WriteLine("Deposited "+amount);
+            }
+            public void Withdrawn(double amount)
+            {
+                if(amount <= balance)
+                {
+                    balance = balance - amount;
+                    Console.WriteLine("Withdrewn" + amount);
+                }
+                else
+                {
+                    Console.WriteLine("Insffiecient Balance");
+                }
+            }
+
+            public void Display()
+            {
+                Console.WriteLine("Account Number :" +accountNumber);
+                Console.WriteLine("Accoutn Holder Name :" + accountHolderName);
+                Console.WriteLine("Balance:" + balance);
+            }
+        }
+        static void Main(string[] args)
+        {
+            BankAccount1 acc1 = new BankAccount1();
+            acc1.accountNumber = 93782;
+            acc1.accountHolderName = "Ram";
+            acc1.balance = 1000;
+
+            acc1.Deposit(2000);
+            acc1.Withdrawn(300);
+            acc1.Display();
+
+            Console.WriteLine("--------------------------------------------");
+            BankAccount1 acc2 = new BankAccount1();
+            acc2.accountNumber = 7895;
+            acc2.accountHolderName = "Jayant";
+            acc2.balance = 6300;
+
+            acc2.Deposit(700);
+            acc2.Withdrawn(1000);
+            acc2.Display();
+
+            Console.WriteLine("--------------------------------------------");
+            BankAccount1 acc3 = new BankAccount1();
+            acc3.accountNumber = 4563;
+            acc3.accountHolderName = "Dipak";
+            acc3.balance = 4500;
+
+            acc3.Deposit(700);
+            acc3.Withdrawn(100);
+            acc3.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Learning_C_Sharp/Students.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_C_Sharp
+{
+    class studetn1001
+    {
+        public int id;
+        public string name;
+        public int age;
+
+        public void Display()
+        {
+            Console.WriteLine($"Student id :{id}");
+            Console.WriteLine("Name is :" +name);
+            Console.WriteLine("Age is :" +age);
+        }
+    }
+
+    class car1
+    {
+        public string brand;
+        public string color;
+        public int maxSpeed;
+
+        public void showDetail()
+        {
+            Console.WriteLine("Brand Name is :" + brand);
+            Console.WriteLine("Brand Name is :" + color);
+            Console.WriteLine("Brand Name is :" + maxSpeed);
+        }
+    }
+    internal class Students
+    {
+        static void Main(string[] args)
+        {
+            // creating a object of Students 1001 class
+            studetn1001 s1 = new studetn1001(); 
+            //Assigning values
+            s1.id = 101;
+            s1.name = "deepak";
+            s1.age = 45;
+            // Calling methods using object
+            s1.Display();
+
+            Console.WriteLine("-------------------------------------------------");
+            studetn1001 s2 = new studetn1001();
+            s2.id = 102;
+            s2.name = "Vishal";
+            s2.age = 63;
+            s2.Display();
+            Console.WriteLine("--------------------- Car -----------------------");
+            car1 c1 = new car1();
+            c1.brand = "BMW";
+            c1.color = "Red";
+            c1.maxSpeed = 350;
+
+            car1 c2 = new car1();
+            c2.brand = "Range Rover";
+            c2.color = "Blue";
+            c2.maxSpeed = 450;
+
+            car1 c3 = new car1();
+            c3.brand = "Rollcy Roy";
+            c3.color = "Black";
+            c3.maxSpeed = 280;
+
+            c1.showDetail();
+            Console.WriteLine("-------------------------------------------------");
+            c2.showDetail();
+            Console.WriteLine("-------------------------------------------------");
+            c3.showDetail();
+            Console.WriteLine("-------------------------------------------------");
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
