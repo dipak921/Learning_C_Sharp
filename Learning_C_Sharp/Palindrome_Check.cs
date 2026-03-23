8c86534fb44b8af514d730321a586c27e4d40600
dipak921 <dipakchande921@gmail.com>
2026-03-23 15:50:32 +0530

    some questinn solve

---
+++ Learning_C_Sharp/Palindrome_Check.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_C_Sharp
+{
+    internal class Palindrome_Check
+    {
+        /*
+            What is palindrome 
+         -> A palindrome is a word that reads the same backward or forward
+            ex. radar, civic,madam, level
+        non-palindrome 
+        dipaK, nikita, sonali, pandurang
+         */
+        static void Main(string[] args)
+        {
+            
+            string r_str = "";
+            Console.WriteLine("Enter a String :");
+            string str = Console.ReadLine();
+            str = str = str.ToLower();// convert all letter in small case
+
+            for (int i = str.Length - 1; i>=0; i--)
+            {
+                r_str += str[i];
+            }
+            // check palindrome
+            if(r_str == str)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not palindrome");
+            }
+           
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Learning_C_Sharp/Swapping.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_C_Sharp
+{
+    internal class Swapping
+    {
+        static void Main(string[] args)
+        {
+            int a = 20; 
+            int b = 30;
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+            a = a + b; // a = 20 + 30, a = 50
+            b = a - b; // b = 50 - 30, b = 20
+            a = a - b; // a = 50 - 20, a = 30
+
+            Console.WriteLine("-------------- Swap Result ------------");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+}
